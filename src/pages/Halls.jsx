aa3448c16bc91104d12c96deb4e37a690eaeb0d9
Volodymyr Piniaha <volodymyr.pinyaha@gmail.com>
2025-12-09 16:06:49 +0200

    Replace mock data with API call for halls
    Fetch halls data from backend API and update rendering.

--- src/pages/Halls.jsx
+++ src/pages/Halls.jsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
 import "./Halls.css";
+import { useEffect, useState } from "react";
 
 export default function Halls() {
-  // Mock data before backend
-  const halls = [
-    { id: 1, name: "Main Sports Hall" },
-    { id: 2, name: "Gym #1" },
-    { id: 3, name: "Aerobic Room" }
-  ];
+  const [halls, setHalls] = useState([]);
+
+  useEffect(() => {
+    fetch("http://<IP_BACKEND>:8000/api/halls/")
+      .then(res => res.json())
+      .then(data => setHalls(data))
+      .catch(err => console.error(err));
+  }, []);
 
   return (
     <div className="page">
       <h1>Available Halls</h1>
-
       <ul className="hall-list">
-        {halls.map((h) => (
-          <li key={h.id}>
-            <Link className="hall-item" to={`/schedule/${h.id}`}>
-              {h.name}
+        {halls.map(h => (
+          <li key={h.HallID}>
+            <Link className="hall-item" to={`/schedule/${h.HallID}`}>
+              {h.Name}
             </Link>
           </li>
         ))}
