5ba28fc63aae504d3a17e9f4a2511845cbd776c6
Volodymyr Piniaha <volodymyr.pinyaha@gmail.com>
2025-12-09 15:29:55 +0200

    Delete src directory

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="page">
-      <h1>Welcome to LNU Booking System</h1>
-      <p>Book halls and sports rooms at the university.</p>
-    </div>
-  );
-}
